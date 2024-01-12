21fd884106174bd37fee0585132e036e109c3c43
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-01-12 10:23:34 +0530

    Kth smallest element

---
+++ Kth smallest element.cpp
@@ -0,0 +1,21 @@
+link-->https://www.geeksforgeeks.org/problems/kth-smallest-element5635/1
+Q.Kth smallest element
+code-->int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        priority_queue<int>pq;
+        for(int i=0;i<k;i++){
+            pq.push(arr[i]);
+        }
+        //
+        for(int i=k;i<=r;i++){
+            if(arr[i]<pq.top()){
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        int ans= pq.top();
+        return ans;
+         
+    }
+Expected Time Complexity: O(n*log(n) )
+Expected Auxiliary Space: O(log(n))
